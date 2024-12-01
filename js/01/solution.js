5317f31abc3ec7d8361cde441094a78f7b87f832
Andrés Pedraza Granados <andrespedrazag@outlook.com>
2024-12-01 17:55:08 -0500

    day one DONE

--- js/01/solution.js
+++ js/01/solution.js
@@ -4,5 +4,39 @@ const readFile = async (path) => {
   return text.trim()
 }
 
-// Assumes you're running in the project's root directory
-readFile("./js/01/input").then(console.log)
+const getLists = async () => {
+  // Assumes you're running in the project's root directory
+  const lines = (await readFile("./js/01/input")).split("\n")
+  const n = lines.length
+  let leftList = new Array(n), rightList = new Array(n)
+
+  for (let i=0; i<n; i++) {
+    const [left, right] = lines[i].split("   ")
+    leftList[i] = parseInt(left)
+    rightList[i] = parseInt(right)
+  }
+
+  return [leftList, rightList]
+}
+
+const part1 = async () => {
+  let [leftList, rightList] = await getLists()
+  leftList.sort((a,b) => a-b)
+  rightList.sort((a,b) => a-b)
+  return leftList.reduce(
+    (dist, left, i) => dist + Math.abs(left - rightList[i]),
+    0
+  )
+}
+
+const part2 = async () => {
+  let [leftList, rightList] = await getLists()
+  return leftList.reduce(
+    (score, left) => score + left * rightList.filter(x => x == left).length,
+    0
+  )
+}
+
+
+part1().then(console.log)
+part2().then(console.log)
--- ocaml/01/solution.ml
+++ ocaml/01/solution.ml
@@ -1,11 +1,45 @@
 let read_file file =
   let ic = open_in file in
   try
-    let text = In_channel.input_all ic in
-    String.trim text
+    String.trim (In_channel.input_all ic)
   with e ->
-    close_in_noerr ic;
-    raise e
+    close_in_noerr ic; raise e;;
 
-(* Assumes you're running in the project's root directory *)
-let () = print_endline (read_file "./ocaml/01/input")
+exception Non_parsable
+
+let get_lists () = 
+  (* Assumes you're running in the project's root directory *)
+  let text = read_file "./ocaml/01/input" in
+  let lines = Str.split (Str.regexp "\n") text in
+  let into_numbers line =
+    match Str.split (Str.regexp " +") line with
+    | x::y::[] -> (int_of_string x, int_of_string y)
+    | _ -> raise Non_parsable in
+  let unzip (ll, rl) (x, y) = (x::ll, y::rl) in
+  List.map into_numbers lines
+    |> List.fold_left unzip ([], []);;
+
+exception Different_lengths
+
+let part1 () =
+  let (left_list, right_list) = get_lists () in
+  let sort l = List.sort compare l in
+  let rec distance paired_lists =
+    match paired_lists with
+    | [], [] -> 0
+    | x::ll, y::rl -> Int.abs (x-y) + distance (ll, rl)
+    | _ -> raise Different_lengths in
+  distance (sort left_list, sort right_list);;
+
+let part2 () = 
+  let (left_list, right_list) = get_lists () in
+  let score elem = right_list
+    |> List.filter (fun x -> x = elem)
+    |> List.length
+    |> ( * ) elem in
+  List.map score left_list
+    |> List.fold_left (+) 0;;
+
+print_int (part1 ());
+print_endline "";
+print_int (part2 ());;
